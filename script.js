32f0f4008afb7660d0b5e8c15a0bf6044d680dda
brijeshg123 <biju.gandalal@gmail.com>
2025-06-08 22:37:57 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function EvaluateSysInfo() {
+  const ram = document.getElementById('ram').value;
+  const cpu = document.getElementById('cpu').value;
+
+  alert(`Selected Configuration:\nRAM: ${ram}\nCPU: ${cpu}`);
+}
